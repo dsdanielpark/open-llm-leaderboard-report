07c75b9f8c7b9c1759a1d183720b563ed3e60207
parkminwoo <parkminwoo1991@gmail.com>
2023-06-01 17:15:57 +0900

    doc: updated

--- execute_steps.py
+++ execute_steps.py
@@ -7,6 +7,8 @@ from src.processing.preprocessor import run_preprocess
 
 
 def run_steps(step_names: list) -> None:
+
+    # Set date and save path
     now = datetime.now()
     formatted_date = now.strftime("%Y%m%d")
     save_path = f"{CONF.SAVE_PATH}/{formatted_date}"
@@ -15,36 +17,25 @@ def run_steps(step_names: list) -> None:
     data_path = f"{CONF.DATA_PATH}/{formatted_date}.json"
     print(f"\n* Plots will be made using {data_path}")
 
+    # Open raw data
     with open(data_path, "r") as f:
         data = json.load(f)
     df = run_preprocess(data)  # Assuming you have a run_preprocess function
     df.to_csv(f"{save_path}/{formatted_date}.csv", index=False, encoding="utf-8-sig")
 
+    # Set metric col
+    metrics = CONF.METRIC_COL
+    
+    # Run steps
     for name in step_names:
         if name == "vis_totalplot":
             vis_totalplot(df, False)
         elif name == "vis_corrheatmap":
             vis_corrheatmap(df)
         elif name == "vis_barplot":
-            metrics = [
-                "Average",
-                "ARC (25-shot)",
-                "HellaSwag (10-shot)",
-                "MMLU (5-shot)",
-                "TruthfulQA (0-shot)",
-                "Parameters",
-            ]
             for col in metrics:
                 vis_barplot(df, col)
         elif name == "vis_rankingplot":
-            metrics = [
-                "Average",
-                "ARC (25-shot)",
-                "HellaSwag (10-shot)",
-                "MMLU (5-shot)",
-                "TruthfulQA (0-shot)",
-                "Parameters",
-            ]
             for col in metrics:
                 vis_rankingplot(df, col)
         elif name == "vis_top10lineplot":
