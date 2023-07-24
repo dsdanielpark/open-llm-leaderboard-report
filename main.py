8c0ef223a1f4e038cfdc32ea270d513140bcdd8f
parkminwoo <parkminwoo1991@gmail.com>
2023-07-24 11:10:45 +0900

    chore: code note

--- main.py
+++ main.py
@@ -6,27 +6,30 @@ SEPERATE_LINE = "=" * 55
 
 
 def main():
+    # Start 
     print("Available step names:")
     for name in STEPS:
         print(name)
     print(SEPERATE_LINE)
     print("Just press enter if you run all steps.")
     print(SEPERATE_LINE)
+
+    # Set steps
     step_input = input("Enter the step names to execute (comma-separated): ")
     print(SEPERATE_LINE)
     if not step_input:
         step_names = STEPS
     else:
         step_names = [step.strip() for step in step_input.split(",")]
-
     print("Selected steps to execute:")
     for name in step_names:
         print(name)
 
+    # Run stepss
     subprocess.run(["python", "execute_steps.py"] + step_names)
-
     run_steps(step_names)
 
+    # Finish
     print("Process Done.")
 
 
